93d696943350a2b7e6fdf13b8c23fc05082c5f75
rrencat <104782737+rrencat@users.noreply.github.com>
2023-08-31 15:47:29 +0200

    logout function

---
+++ src/js/handlers/auth/logoutListener.js
@@ -0,0 +1,15 @@
+import * as storage from "../../services/storage.js";
+
+export function logoutListener() {
+    const logoutButton = document.querySelector("#logout");
+
+    logoutButton.addEventListener("click", () => {
+        storage.remove("token");
+        storage.remove("name");
+        storage.remove("profile");
+    });
+
+    if (logoutListener.ok) {
+        window.location = "/";
+    }
+}
--- src/js/router.js
+++ src/js/router.js
@@ -1,14 +1,23 @@
 import * as handlers from "./handlers/index.js";
 
-export default function router() {
-    const path = location.pathname;
 
-    switch (path) {
+export default function router() {
+    const pathname = window.location.pathname;
+
+    
+
+    switch (pathname) {
+        case "/":
+        case "index.html":
+            break;
         case "/profile/register/":
             handlers.setRegisterFormListener()
             break; 
         case "/profile/login/":
             handlers.setLoginFormListener()
+            break;
+        case "/profile/":
+            handlers.logoutListener()
             break;
     } 
 }

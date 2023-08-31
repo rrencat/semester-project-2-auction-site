c1da6ee6ab1bd285e200582642764d76a186fb1c
rrencat <104782737+rrencat@users.noreply.github.com>
2023-08-31 13:03:37 +0200

    changed name from listeners to handlers

--- src/js/router.js
+++ src/js/router.js
@@ -1,14 +1,14 @@
-import * as listeners from "./handlers/index.js";
+import * as handlers from "./handlers/index.js";
 
 export default function router() {
     const path = location.pathname;
 
     switch (path) {
         case "/profile/register/":
-            listeners.setRegisterFormListener()
+            handlers.setRegisterFormListener()
             break; 
         case "/profile/login/":
-            listeners.setLoginFormListener()
+            handlers.setLoginFormListener()
             break;
     } 
 }
